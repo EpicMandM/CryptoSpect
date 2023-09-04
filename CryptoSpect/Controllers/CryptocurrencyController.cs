84b62aa3fde279dd24bf0d6ada71874d693c0556
Zhukov Vladislav <epicmand.mi@gmail.com>
2023-09-04 14:49:16 +0300

    feat: add cryptocurrency controller

--- CryptoSpect/Controllers/CryptocurrencyController.cs
+++ CryptoSpect/Controllers/CryptocurrencyController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using CryptoSpect.Service.Interfaces;
 using CryptoSpect.Core.Models;
+using CryptoSpect.Utility.Logging;
 
 namespace CryptoSpect.Controllers;
 
@@ -9,26 +10,47 @@ namespace CryptoSpect.Controllers;
 /// </summary>
 [ApiController]
 [Route("api/[controller]")]
-public class CryptocurrencyController : ControllerBase
+public sealed class CryptocurrencyController : ControllerBase
 {
     private readonly ICryptocurrencyService _cryptocurrencyService;
+    private readonly ILogger<CryptocurrencyController> _logger;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="CryptocurrencyController"/> class.
     /// </summary>
-    /// <param name="cryptocurrencyService">The cryptocurrency service.</param>
-    public CryptocurrencyController(ICryptocurrencyService cryptocurrencyService)
+    /// <param name="cryptocurrencyService">The service responsible for handling cryptocurrency operations.</param>
+    /// <param name="logger">The logger used to log events and issues related to the CryptocurrencyController.</param>
+    public CryptocurrencyController(ICryptocurrencyService cryptocurrencyService, ILogger<CryptocurrencyController> logger)
     {
         _cryptocurrencyService = cryptocurrencyService;
+        _logger = logger;
     }
 
     /// <summary>
     /// Retrieves all cryptocurrencies.
     /// </summary>
     [HttpGet]
-    public async Task<IActionResult> GetAsync()
+    public async Task<IActionResult> GetAllAsync()
     {
-        return Ok(await _cryptocurrencyService.GetAllAsync().ConfigureAwait(false));
+        try
+        {
+            LogEvents.LogGetAllCryptocurrenciesStarted(_logger, arg2: null);
+
+            var cryptocurrencies = await _cryptocurrencyService.GetAllAsync().ConfigureAwait(false);
+
+            if (cryptocurrencies?.Any() ?? true)
+            {
+                return NotFound();
+            }
+
+            LogEvents.LogGetAllCryptocurrenciesCompleted(_logger, cryptocurrencies.Count(), arg3: null);
+            return Ok(cryptocurrencies);
+        }
+        catch (Exception e)
+        {
+            LogEvents.LogUnexpectedError(_logger, e);
+            return StatusCode(500, "Internal server error");
+        }
     }
 
     /// <summary>
@@ -38,22 +60,46 @@ public class CryptocurrencyController : ControllerBase
     [HttpGet("{id}")]
     public async Task<IActionResult> GetByIdAsync(string id)
     {
-        var cryptocurrency = await _cryptocurrencyService.GetByIdAsync(id).ConfigureAwait(false);
-        if (cryptocurrency is null)
-            return NotFound();
+        try
+        {
+            var cryptocurrency = await _cryptocurrencyService.GetByIdAsync(id).ConfigureAwait(false);
+            if (cryptocurrency is null)
+            {
+                LogEvents.LogCryptocurrencyNotFound(_logger, id, arg3: null);
+                return NotFound();
+            }
 
-        return Ok(cryptocurrency);
+            return Ok(cryptocurrency);
+        }
+        catch (Exception e)
+        {
+            LogEvents.LogUnexpectedError(_logger, e);
+            return StatusCode(500, "Internal server error");
+        }
     }
-
     /// <summary>
     /// Adds a new cryptocurrency.
     /// </summary>
     /// <param name="newCryptocurrency">The cryptocurrency to add.</param>
     [HttpPost]
-    public async Task<IActionResult> Add([FromBody] Cryptocurrency newCryptocurrency)
+    public async Task<IActionResult> AddAsync([FromBody] Cryptocurrency newCryptocurrency)
     {
-        await _cryptocurrencyService.AddAsync(newCryptocurrency).ConfigureAwait(false);
-        return CreatedAtAction(nameof(GetByIdAsync), new { id = newCryptocurrency.Id }, newCryptocurrency);
+        if (newCryptocurrency is null)
+        {
+            LogEvents.LogNullCryptocurrency(_logger, arg2: null);
+            return BadRequest();
+        }
+        try
+        {
+            await _cryptocurrencyService.AddAsync(newCryptocurrency).ConfigureAwait(false);
+            LogEvents.LogCryptocurrencyAdded(_logger, newCryptocurrency.Id, arg3: null);
+            return CreatedAtAction(nameof(GetByIdAsync), new { id = newCryptocurrency.Id }, newCryptocurrency);
+        }
+        catch (Exception e)
+        {
+            LogEvents.LogUnexpectedError(_logger, e);
+            return StatusCode(500, "Internal server error");
+        }
     }
 
     /// <summary>
@@ -62,13 +108,29 @@ public class CryptocurrencyController : ControllerBase
     /// <param name="id">The unique identifier for the cryptocurrency to update.</param>
     /// <param name="updatedCryptocurrency">The updated cryptocurrency data.</param>
     [HttpPut("{id}")]
-    public async Task<IActionResult> Update(string id, [FromBody] Cryptocurrency updatedCryptocurrency)
+    public async Task<IActionResult> UpdateAsync(string id, [FromBody] Cryptocurrency updatedCryptocurrency)
     {
-        if (!string.Equals(id, updatedCryptocurrency.Id, StringComparison.Ordinal))
+        if (updatedCryptocurrency is null)
+        {
+            LogEvents.LogNullCryptocurrency(_logger, arg2: null);
             return BadRequest();
-
-        await _cryptocurrencyService.UpdateAsync(updatedCryptocurrency).ConfigureAwait(false);
-        return NoContent();
+        }
+        if (!string.Equals(id, updatedCryptocurrency.Id, StringComparison.Ordinal))
+        {
+            LogEvents.LogCryptocurrencyNotFound(_logger, updatedCryptocurrency.Id, arg3: null);
+            return BadRequest();
+        }
+        try
+        {
+            await _cryptocurrencyService.UpdateAsync(updatedCryptocurrency!).ConfigureAwait(false);
+            LogEvents.LogCryptocurrencyUpdated(_logger, updatedCryptocurrency.Id, arg3: null);
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            LogEvents.LogUnexpectedError(_logger, e);
+            return StatusCode(500, "Internal server error");
+        }
     }
 
     /// <summary>
@@ -76,9 +138,18 @@ public class CryptocurrencyController : ControllerBase
     /// </summary>
     /// <param name="id">The unique identifier for the cryptocurrency to delete.</param>
     [HttpDelete("{id}")]
-    public async Task<IActionResult> Delete(string id)
+    public async Task<IActionResult> DeleteAsync(string id)
     {
-        await _cryptocurrencyService.DeleteAsync(id).ConfigureAwait(false);
-        return NoContent();
+        try
+        {
+            await _cryptocurrencyService.DeleteAsync(id).ConfigureAwait(false);
+            LogEvents.LogCryptocurrencyDeleted(_logger, id, arg3: null);
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            LogEvents.LogUnexpectedError(_logger, e);
+            return StatusCode(500, "Internal server error");
+        }
     }
 }
